9470223f85fa5ba042725c967be0d81dec98f5df
mara <ms.marafinkel@gmail.com>
2021-12-18 21:26:56 -0800

    Add signup form container

---
+++ frontend/components/session_form/signup_form_container.jsx
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { signup } from '../../actions/session_actions';
+import SessionForm from './session_form';
+import { login } from '../../actions/session_actions';
+
+const mapStateToProps = ({ errors }) => {
+  return {
+    errors: errors.session,
+    formType: 'Sign up',
+    navLink: <Link to="/login">Log in</Link>,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: (user) => dispatch(signup(user)),
+    guestLogin: (user) => dispatch(login(user))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
