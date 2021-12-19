bf7120dd494168c2114b19d169e659931013fc74
mara <ms.marafinkel@gmail.com>
2021-12-18 20:26:13 -0800

    Populate sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,32 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid username/password combination you dumb whore"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["You need to sign the fuck in ho"], status: 404
+    end
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:username, :password)
+  end
 end
+
